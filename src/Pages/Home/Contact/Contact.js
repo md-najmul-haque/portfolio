5783b36e0e4a90cb008595d4be2b7adab6578f34
najuml-bd <najmul0612@gmail.com>
2022-06-09 21:31:59 +0600

    contact form connected to email

--- src/Pages/Home/Contact/Contact.js
+++ src/Pages/Home/Contact/Contact.js
@@ -1,34 +1,17 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { toast } from 'react-toastify';
+import emailjs from 'emailjs-com';
 
 
 const Contact = () => {
 
     const { register, formState: { errors }, handleSubmit, reset } = useForm();
 
-    const onSubmit = data => {
+    const onSubmit = (data, e) => {
 
-        const email = {
-            customerName: data.name,
-            email: data.email,
-            phone: data.phone,
-            address: data.address,
-            message: data.message
-        }
-        fetch(`https://serene-wave-89546.herokuapp.com/contact`, {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(email)
-        })
-            .then(res => res.json())
-            .then(result => {
-
-                console.log(result)
-            });
-        toast(`Thanks for your email`)
+        emailjs.sendForm('service_t2yjpt7', 'template_m6usr6m', e.target, '05q1Ckj6xqf-KKHgb')
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
         reset();
 
     };
@@ -37,23 +20,36 @@ const Contact = () => {
 
         <div className="hero contact-section min-h-fit pt-20 pb-40" >
 
+            <div class="card w-96 bg-primary text-primary-content">
+                <div class="card-body">
+                    <div>
+                        <div>
+
+                        </div>
+                        <div>
+                            <h2 class="card-title">Phone</h2>
+                            <p>+8801717888789</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
 
             <div className="card-body w-96">
-                <h2 className="text-3xl text-primary font-bold">Request A Quote</h2>
-                <p>We ensure our customers receive the best quality prices and service. Feel free to mail us </p>
+                <h2 className="text-3xl text-primary font-bold">Get in Touch</h2>
 
                 <form className='gap-1' onSubmit={handleSubmit(onSubmit)}>
 
                     <input
                         type="text"
                         placeholder="Enter Your Name"
-                        className="input bg-base-200  border-2 focus:border-lime-600 rounded-3xl w-full max-w-xs md:mr-7 mb-2"
+                        className="input input-bordered w-full max-w-xs md:mr-7 mb-2"
                         {...register("name")} />
 
                     <input
                         type="text"
                         placeholder="Enter Your Email"
-                        className="input bg-base-200 rounded-3xl border-2 focus:border-lime-600  w-full max-w-xs mb-2"
+                        className="input input-bordered w-full max-w-xs mb-2"
                         {...register("email", {
                             required: {
                                 value: true,
@@ -73,13 +69,13 @@ const Contact = () => {
                     <input
                         type="text"
                         placeholder="Please white subject here"
-                        className="input bg-base-200 outline-0  border-2 focus:border-lime-600 rounded-3xl w-full max-w-xs mb-2"
+                        className="input input-bordered w-full max-w-xs mb-2"
                         {...register("subject")} />
 
                     <input
                         type="textarea"
                         placeholder="Leave Your message here"
-                        className="input bg-base-200 border-2 focus:border-lime-600  rounded-xl w-full h-36 max-w-xs lg:max-w-2xl my-3"
+                        className="input input-bordered w-full h-36 max-w-xs lg:max-w-2xl my-3"
                         {...register("message", {
                             required: {
                                 value: true,
@@ -91,7 +87,7 @@ const Contact = () => {
                         {errors.message?.type === 'required' && <span className='text-red-600'>{errors.message.message}</span>}
                     </label>
 
-                    <button type="submit" className="btn max-w-xs btn-primary  bg-gradient-to-r from-secondary to-primary  text-white">Submit Request <span className='ml-2'></span></button>
+                    <button type="submit" className="btn max-w-xs btn-primary  bg-gradient-to-r from-secondary to-primary  text-white">Submit</button>
 
                 </form>
 
