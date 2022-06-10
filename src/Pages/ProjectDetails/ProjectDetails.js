ee66b56d1a2d2cab0d5d05d6197cde1c83e00c3d
najuml-bd <najmul0612@gmail.com>
2022-06-10 23:56:39 +0600

    completed

--- src/Pages/ProjectDetails/ProjectDetails.js
+++ src/Pages/ProjectDetails/ProjectDetails.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
-const ProjectDetails = () => {
+const ProjectDetails = (props) => {
     const [project, setProject] = useState([])
     const { id } = useParams()
 
@@ -14,15 +15,60 @@ const ProjectDetails = () => {
 
     const { _id, name, img, screenshot1, screenshot2, screenshot3, screenshot4, screenshot5, screenshot6, clientRepo, serverRepo, liveLink, feature1, feature2, feature3, feature4, feature5, feature6 } = project
 
+    const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 1,
+            slidesToSlide: 1
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 600 },
+            items: 1,
+            slidesToSlide: 1
+        },
+        mobile: {
+            breakpoint: { max: 600, min: 0 },
+            items: 1,
+            slidesToSlide: 1
+        }
+    };
+
     return (
         <div className=''>
 
             <h1 className='text-2xl md:text-5xl text-center font-semibold my-5'>{name} Projects Details</h1>
 
             <div class="card w-full md:w-7/12 bg-base-100 shadow-2xl mx-auto">
-                <figure class="px-10 pt-10">
-                    <img src={img} alt="Shoes" />
-                </figure>
+                <Carousel
+                    swipeable={false}
+                    draggable={false}
+                    showDots={true}
+                    responsive={responsive}
+                    ssr={true}
+                    infinite={true}
+                    autoPlay={props.deviceType !== "mobile" ? true : false}
+                    autoPlaySpeed={2500}
+                    keyBoardControl={true}
+                    customTransition="all .5"
+                    transitionDuration={500}
+                    containerClass="carousel-container"
+                    removeArrowOnDeviceType={["tablet", "mobile"]}
+                    deviceType={props.deviceType}
+                    dotListClass="custom-dot-list-style"
+                    itemClass="carousel-item-padding-40-px"
+                >
+
+                    <div><img src={screenshot1} alt="Shoes" /></div>
+                    <div><img src={screenshot2} alt="Shoes" /></div>
+                    <div><img src={screenshot3} alt="Shoes" /></div>
+                    <div><img src={screenshot4} alt="Shoes" /></div>
+                    <div><img src={screenshot5} alt="Shoes" /></div>
+                    <div><img src={screenshot5} alt="Shoes" /></div>
+                    <div><img src={screenshot6} alt="Shoes" /></div>
+
+
+                </Carousel>
+
                 <div class="card-body items-center text-center">
                     <h2 class="card-title text-3xl">{name}</h2>
                     <ul className='text-left list-disc p-7'>
