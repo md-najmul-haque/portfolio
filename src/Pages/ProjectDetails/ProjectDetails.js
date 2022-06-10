ec46f1ebea5742fdc184578776ceb6e4aef77cc5
najuml-bd <najmul0612@gmail.com>
2022-06-10 20:11:45 +0600

    project detail card added

--- src/Pages/ProjectDetails/ProjectDetails.js
+++ src/Pages/ProjectDetails/ProjectDetails.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 
 const ProjectDetails = () => {
@@ -9,13 +10,30 @@ const ProjectDetails = () => {
         fetch(`http://localhost:5000/projects/${id}`)
             .then(res => res.json())
             .then(data => setProject(data))
-    }, [id])
+    }, [])
+
+    const { _id, img, name, description, liveLink } = project
 
     return (
-        <div>
-            <h2>Projects: {id}</h2>
-            <h2>Name:{project.name}</h2>
+        <div className=''>
 
+            <h1 className='text-2xl md:text-5xl text-center font-semibold my-5'>{name} Projects Details</h1>
+
+            <div class="card w-96 bg-base-100 shadow-xl mx-auto">
+                <figure class="px-10 pt-10">
+                    <img src={img} alt="Shoes" />
+                </figure>
+                <div class="card-body items-center text-center">
+                    <h2 class="card-title">{name}</h2>
+                    <p>{description}</p>
+
+                    <div className='mt-5'>
+                        <Link class="btn btn-primary ml-3" to={`/projects/${_id}`}>Explore Detail</Link>
+                        <a class="btn btn-primary ml-3" href={liveLink} rel="noreferrer" role='button' target='_blank'>Live Site</a>
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 };
