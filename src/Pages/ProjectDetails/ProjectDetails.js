c5116a50c907aa011661e6da1819c48343cd796f
najuml-bd <najmul0612@gmail.com>
2022-06-10 20:24:08 +0600

    project detail card final

--- src/Pages/ProjectDetails/ProjectDetails.js
+++ src/Pages/ProjectDetails/ProjectDetails.js
@@ -19,7 +19,7 @@ const ProjectDetails = () => {
 
             <h1 className='text-2xl md:text-5xl text-center font-semibold my-5'>{name} Projects Details</h1>
 
-            <div class="card w-96 bg-base-100 shadow-xl mx-auto">
+            <div class="card w-full md:w-7/12 bg-base-100 shadow-2xl mx-auto">
                 <figure class="px-10 pt-10">
                     <img src={img} alt="Shoes" />
                 </figure>
@@ -27,8 +27,9 @@ const ProjectDetails = () => {
                     <h2 class="card-title">{name}</h2>
                     <p>{description}</p>
 
-                    <div className='mt-5'>
-                        <Link class="btn btn-primary ml-3" to={`/projects/${_id}`}>Explore Detail</Link>
+                    <div className='flex flex-col md:flex-row gap-3 mt-5'>
+                        <a class="btn btn-primary" href={liveLink} rel="noreferrer" role='button' target='_blank'>GitHub Client Side Code</a>
+                        <a class="btn btn-primary ml-3" href={liveLink} rel="noreferrer" role='button' target='_blank'>GitHub Client Side Code</a>
                         <a class="btn btn-primary ml-3" href={liveLink} rel="noreferrer" role='button' target='_blank'>Live Site</a>
                     </div>
 
